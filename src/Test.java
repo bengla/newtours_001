1c4ea11b8d990f211164699f21b1f898ab756f04
bengla <zaman.nuru321@gmail.com>
2017-12-18 19:13:45 -0500

    checkin

--- src/Test.java
+++ src/Test.java
@@ -100,5 +100,7 @@ public class Test {
 	   // browsers.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[7]/td/table/tbody/tr/td[3]/a/img"));
 	    //browsers.get("http://newtours.demoaut.com/mercurypurchase2.php");
 	    //checkout
+	    //checkin
+	    //check ==
 	}
 }
